7f091b30dc07b9eb1450ea627c762725db9b7d8d
Robbie Laughlen <robbity@students.cs.ubc.ca>
2024-01-21 00:24:35 -0800

    Filled LoadingScreen Info

--- Echo/src/screens/LoadingScreen.js
+++ Echo/src/screens/LoadingScreen.js
@@ -1,25 +1,61 @@
-// src/screens/ProfileScreen.js
+import React from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const LoadingScreen = () => {
+const LoadingScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Hold on tight while we do our thing...</Text>
-        </View>
+      <Image
+        resizeMode="contain"
+        source={{
+          uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/37353037-f0e7-4f2c-9879-6987bc4b4fcd?apiKey=a65f153f0616484bbdddb43b863be0fa&",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <Text>Hold on tight while we do our thing...</Text>
+      </View>
+    </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+
+        backgroundColor: "#FFF",
+        width: "100%",
+        height: "100%",
+        padding: 50,
+
+      backgroundColor: "#FFF",
+      display: "flex",
+      maxWidth: 480,
+      width: "100%",
+      flexDirection: "column",
+      alignItems: "stretch",
+      margin: "0 auto",
+      padding: 50,
     },
-    text: {
-        fontSize: 20
-    }
-});
+    image: {
+      overflow: "hidden",
+      position: "relative",
+      display: "flex",
+    //   marginTop: 243,
+      flexDirection: "column",
+      aspectRatio: 1.18,
+    },
+    content: {
+      color: "var(--black, #000C34)",
+      textAlign: "center",
+      lineHeightTrim: "both",
+      textEmphasis: "filled",
+      marginVertical: 8,
+    //   marginBottom: 180,
+      fontWeight: "500",
+      fontSize: 25,
+      lineHeight: 28,
+      fontFamily: "Nunito, sans-serif",
+    },
+  });
 
 export default LoadingScreen;
+
