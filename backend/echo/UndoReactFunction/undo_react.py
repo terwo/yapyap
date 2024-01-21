2efcc9fd50bfa43488442e9039169f728c7b6af0
act18 <alvin_chun-yum_tsang@sfu.ca>
2024-01-20 17:20:17 -0800

    add unreact lambda function

--- backend/echo/UndoReactFunction/undo_react.py
+++ backend/echo/UndoReactFunction/undo_react.py
@@ -6,9 +6,8 @@ def lambda_handler(event, context):
     This lambda function does the following:
     * Authenticate the user #TODO
     * Extract the post_id, user_id and reaction from event[body] #TODO
-    * Check if the user has already reacted to the post. 
-      If not, update the Post object in the database #TODO
-    * Signal the user of the post to get a reaction notification #TODO
+    * Check if the user has reacted to the post. 
+      If so, update the Post object in the database #TODO
     Returns:
         Return a message
     """
@@ -17,7 +16,7 @@ def lambda_handler(event, context):
     
     # extract the post_id and reaction for the event[body]
     
-    # check to see if the user has already reacted to this post
+    # check to see if the user has reacted to this post
     
     # update the Post object reaction field
     
