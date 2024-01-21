b759dd6b6c7f8bdafcc8fa8488ef23823088fadf
act18 <alvin_chun-yum_tsang@sfu.ca>
2024-01-20 16:16:06 -0800

    add react lambda function

---
+++ backend/echo/ReactFunction/react.py
@@ -0,0 +1,22 @@
+import json
+
+
+def lambda_handler(event, context):
+    """
+    This lambda function does the following:
+    * Authenticate the user #TODO
+    * Extract the post_id and reaction from event[body] #TODO
+    * Update the Post object in the database #TODO
+    * Signal the user of the post to get a reaction notification #TODO
+    Returns:
+        Return a message
+    """
+
+    # authenticate the user
+    
+    # extract the post_id and reaction for the event[body]
+
+    # update the Post object reaction field
+    
+    #! dummy data
+    return {"statusCode": 200}
