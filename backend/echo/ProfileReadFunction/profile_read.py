40e791ccb6519123a99116edb2b092817b3282a4
act18 <alvin_chun-yum_tsang@sfu.ca>
2024-01-21 02:16:16 -0800

    add profile-read function

--- backend/echo/ProfileReadFunction/profile_read.py
+++ backend/echo/ProfileReadFunction/profile_read.py
@@ -1,31 +1,47 @@
 import json
+import os
+
+from mongoDB import user_profile
 
 
 def lambda_handler(event, context):
     """
-    This lambda function does the following:
-    * Authenticate the user #TODO
-    * Get the user information from the authentication #TODO
-    * Get the User object from the database #TODO
-    Returns:
-        Return a User Object
+    Get the profile of the user
+    Return a User Object
     """
-
+    event = json.loads(event["body"]) if "body" in event else event 
+    
     # authenticate the user
-    
+    if (username := event.get("username")) is None:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"message": f"A username must be included {event}"})
+        }
+
+    if (password := event.get("password")) is None:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"message": "A password must be included"})
+        }
+                
     # get the user information from the authentication
+    result = user_profile(os.environ.get("ATLAS_URI"), username, password)
     
-    # get the user object from the database
-    
-    #! dummy data
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({
-            "user": {
-                "user_id": "1",
-                "posts": ["1", "2", "4536", "58888"],
-                "avatar": "HappyLama"
-            }
-        })
-    }
+    if isinstance(result, dict):
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "user": result
+            })
+        }
+    else:
+        return {
+            "statusCode": 401,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "message": "Unauthorized user"
+            })
+        }
--- backend/echo/layers/python/mongoDB.py
+++ backend/echo/layers/python/mongoDB.py
@@ -35,3 +35,55 @@ def add_user(atlas_uri: str, username: str, password:str) -> str | int:
     })            
 
     return str(result.inserted_id) if result.inserted_id else -2       
+
+
+def login_user(atlas_uri: str, username: str, password:str) -> str | int:
+    """
+    Validate a user into the database.
+
+    Args:
+        username (str): Unique username of the user.
+        password (str): Password for the user.
+
+    Returns:
+        
+    """
+    client = get_client(atlas_uri)
+    users = client.EchoDev.users
+
+    if users.find_one({"username": username}) is None:
+        return -1
+    
+    result = users.insert_one({
+        "username": username,
+        "password": password,
+        "avatar": AVATARS[random.randint(0, len(AVATARS) - 1)],
+        "posts": []
+    })            
+
+    return str(result.inserted_id) if result.inserted_id else -2     
+
+
+def user_profile(atlas_uri: str, username: str, password:str) -> str | bool:
+    """
+    Get the user profile
+
+    Args:
+        username (str): Unique name of the user.
+        password (str): Password for the user.
+
+    Returns:
+        str | bool: user object if log in was successful, False otherwise.
+    """
+    client = get_client(atlas_uri)
+    users = client.EchoDev.users
+
+    if (user := users.find_one({"username": username})) is None:
+        return False
+   
+    user["_id"] = str(user["_id"])
+    
+    for i, post in enumerate(user["posts"]):
+        user["posts"][i] = str(post)
+ 
+    return user
