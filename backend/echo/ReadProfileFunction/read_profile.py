d2758cdf21497e7d94963b8827c60324abf331a8
act18 <alvin_chun-yum_tsang@sfu.ca>
2024-02-04 17:58:28 -0800

    refactor read profile and add tests

---
+++ backend/echo/ReadProfileFunction/read_profile.py
@@ -0,0 +1,50 @@
+import json
+
+def lambda_handler(event, context):
+    """
+    Returns:
+        Get an existing user's information.
+    """
+    if (qsp := event.get("queryStringParameters")) is None:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(
+                {
+                    "message": "Invalid request! Request must include "
+                                + "a query string with the user_id"
+                }
+            )
+        }   
+        
+    if (user_id := qsp.get("user_id")) is None:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(
+                {
+                    "message": "Request must include the user_id "
+                                + "in the query string parameters"
+                }
+            )
+        }   
+    
+    #TODO: check if username exists
+    #TODO: get data from database
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            { # TODO: temp; to be deleted
+                "message": f"Data recieved: user_id={user_id}. " 
+                            + "NOTE: This data does not come from a database.",
+                "avatar": "duck",
+                "posts": []
+            }
+        )
+    }
+    
+    
+    
+    
+    
